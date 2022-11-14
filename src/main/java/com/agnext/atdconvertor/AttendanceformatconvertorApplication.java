7b81b53e79966371f151a94e53d944002a7f1a9d
mitesh-agnext <68939587+mitesh-agnext@users.noreply.github.com>
2022-11-14 19:45:10 +0400

    Initial commit

---
+++ src/main/java/com/agnext/atdconvertor/AttendanceformatconvertorApplication.java
@@ -0,0 +1,13 @@
+package com.agnext.atdconvertor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendanceformatconvertorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttendanceformatconvertorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agnext/atdconvertor/AttendanceformatconvertorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agnext.atdconvertor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendanceformatconvertorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
